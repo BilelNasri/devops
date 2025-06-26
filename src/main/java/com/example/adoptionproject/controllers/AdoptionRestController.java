2dc2007ad25fca431f6ebb57a0f3aa60cec98ac9
BilelNasri <nasri.bilel@esprit.tn>
2025-06-26 17:02:15 +0100

    Update AdoptionRestController.java uses of dto

--- src/main/java/com/example/adoptionproject/controllers/AdoptionRestController.java
+++ src/main/java/com/example/adoptionproject/controllers/AdoptionRestController.java
@@ -1,6 +1,11 @@
 package com.example.adoptionproject.controllers;
 
-import com.example.adoptionproject.entities.*;
+import com.example.adoptionproject.dto.AdoptantDTO;
+import com.example.adoptionproject.dto.AdoptionDTO;
+import com.example.adoptionproject.dto.AnimalDTO;
+import com.example.adoptionproject.entities.Adoptant;
+import com.example.adoptionproject.entities.Adoption;
+import com.example.adoptionproject.entities.Animal;
 import com.example.adoptionproject.services.IAdoptionServices;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.*;
@@ -15,19 +20,31 @@ public class AdoptionRestController {
     private final IAdoptionServices adoptionServices;
 
     @PostMapping("/addAdoptant")
-    public Adoptant addAdoptant(@RequestBody Adoptant adoptant) {
+    public Adoptant addAdoptant(@RequestBody AdoptantDTO dto) {
+        Adoptant adoptant = new Adoptant();
+        adoptant.setNom(dto.getNom());
+        adoptant.setAdresse(dto.getAdresse());
+        adoptant.setTelephone(dto.getTelephone());
         return adoptionServices.addAdoptant(adoptant);
     }
 
     @PostMapping("/addAnimal")
-    public Animal addAnimal(@RequestBody Animal animal) {
+    public Animal addAnimal(@RequestBody AnimalDTO dto) {
+        Animal animal = new Animal();
+        animal.setNom(dto.getNom());
+        animal.setAge(dto.getAge());
+        animal.setSterilise(dto.isSterilise());
+        animal.setEspece(dto.getEspece());
         return adoptionServices.addAnimal(animal);
     }
 
     @PostMapping("/addAdoption/{idAdoptant}/{idAnimal}")
-    public Adoption addAdoption(@RequestBody Adoption adoption,
+    public Adoption addAdoption(@RequestBody AdoptionDTO dto,
                                 @PathVariable int idAdoptant,
                                 @PathVariable int idAnimal) {
+        Adoption adoption = new Adoption();
+        adoption.setDateAdoption(dto.getDateAdoption());
+        adoption.setFrais(dto.getFrais());
         return adoptionServices.addAdoption(adoption, idAdoptant, idAnimal);
     }
 
